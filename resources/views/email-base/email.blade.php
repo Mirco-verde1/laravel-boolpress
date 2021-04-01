97a7c3818db83102fbbcadab3168b466e7272a9d
mirco verderosa <verderosamircowork@gmail.com>
2021-04-01 17:25:28 +0200

    Added in sent  email  info about author

--- resources/views/email-base/email.blade.php
+++ resources/views/email-base/email.blade.php
@@ -1,1 +1,18 @@
-Questa è un mail solo per stressarti
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+       <div>Buongiorno</div>
+
+        Con la presente volevamo avvisarla della creazione di un nuovo post da parte dell'utente : {{$post->author->name}} {{$post->author->surname}}.
+    <div>Aggiunto il : {{$post->created_at}}</div>
+    <div></div>
+    </p>
+</body>
+</html>
